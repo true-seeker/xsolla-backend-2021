067533e1d00523541b95ff71a78179cb4e42dbff
TrueSeeker322 <murz.kirill@yandex.ru>
2021-06-21 14:02:09 +0500

    Product model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    stock_keeping_unit = models.CharField(max_length=30,
+                                          verbose_name='SKU',
+                                          unique=True,
+                                          null=False)
+    title = models.CharField(max_length=200,
+                             verbose_name='Имя',
+                             null=False)
+    product_type = models.ForeignKey('main_app.ProductType',
+                                     on_delete=models.SET_NULL,
+                                     null=True,
+                                     verbose_name='Тип')
+    price = models.FloatField(verbose_name='Стоимость')
+
+
+class ProductType(models.Model):
+    title = models.CharField(max_length=30)
