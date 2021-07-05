90b031baf11529a5b312f789a0ded45eec6d1e4c
TrueSeeker322 <murz.kirill@yandex.ru>
2021-07-05 17:27:07 +0500

    config

--- xsolla_backend_2021/settings.py
+++ xsolla_backend_2021/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
