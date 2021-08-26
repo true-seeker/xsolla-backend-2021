e2524367dd3c17e6077df7ddd0d842a8667a788a
true-seeker <murz.kirill@yandex.ru>
2021-08-26 16:12:18 +0500

    mongodb implemented

--- xsolla_backend_2021/settings.py
+++ xsolla_backend_2021/settings.py
@@ -68,8 +68,8 @@ WSGI_APPLICATION = 'xsolla_backend_2021.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'db-name',
     }
 }
 
